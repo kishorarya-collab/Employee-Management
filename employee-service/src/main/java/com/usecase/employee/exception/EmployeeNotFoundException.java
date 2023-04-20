a96fd691f82c395ab89487181f14e04b0c4ed2b5
Kishor Arya <kishorarya.che18@itbhu.ac.in>
2023-04-20 13:33:21 +0530

    use case

---
+++ employee-service/src/main/java/com/usecase/employee/exception/EmployeeNotFoundException.java
@@ -0,0 +1,11 @@
+package com.usecase.employee.exception;
+
+public class EmployeeNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+    public EmployeeNotFoundException() {
+    	super();
+    }
+    public EmployeeNotFoundException(String msg) {
+    	super(msg);
+    }
+}
